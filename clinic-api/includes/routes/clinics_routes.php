b24f75b77a565a87d746580d53c7c7e0055f9765
Benny Liu <1456415@edu.vaniercollege.qc.ca>
2022-11-14 10:53:11 -0500

    Fixed #3, Closed #4

--- clinic-api/includes/routes/clinics_routes.php
+++ clinic-api/includes/routes/clinics_routes.php
@@ -47,28 +47,82 @@ function handleCreateClinics(Request $request,Response $response, array $args){
     $requested_format = $request->getHeader('Accept');
     
     if($parse_data != null){
+
         foreach($parse_data as $clinic){
-            $clinics = array("clinic_name" => $clinic['clinic_name'] , "clinic_address" => $clinic['clinic_address'],'clinic_details' => $clinic['clinic_details']);
-            $clinic_model->createClinic($clinic);
-        }
-    
-        if ($requested_format[0] === APP_MEDIA_TYPE_JSON) {
-            $response_data = makeCustomJSONMessage("Created","Record(s) has been successfully created.");
-        } else {
-            $response_data = json_encode(getErrorUnsupportedFormat());
-            $response_code = HTTP_UNSUPPORTED_MEDIA_TYPE;
-        }
-    }
-    else{
-        if ($requested_format[0] === APP_MEDIA_TYPE_JSON) {
-            $response_data = makeCustomJSONMessage("Error","Record(s) has not been created.");
-        } else {
-            $response_data = json_encode(getErrorUnsupportedFormat());
-            $response_code = HTTP_UNSUPPORTED_MEDIA_TYPE;
+            if(!isset($clinic['clinic_name']) || !isset($clinic['clinic_address']) || !isset($clinic['clinic_details']) ){
+                $response_data = makeCustomJSONMessage("Error","Record(s) has not been updated. Some fields are empty.");
+                $response_code = HTTP_BAD_REQUEST;
+
+                $response->getBody()->write($response_data);
+                return $response->withStatus($response_code);
+            }
+            else{
+                $clinics = array("clinic_name" => $clinic['clinic_name'] , "clinic_address" => $clinic['clinic_address'],'clinic_details' => $clinic['clinic_details']);
+                $clinic_model->createClinic($clinic);
+            }
+            
         }
 
-        $response_code = HTTP_BAD_REQUEST;
+        $response_data = makeCustomJSONMessage("Created","Record(s) has been successfully created.");
     }
+    else{
+
+        $response_data = makeCustomJSONMessage("Error","Record(s) has not been created. All fields are empty.");
+        $response_code = HTTP_BAD_REQUEST;
+
+    }
+
+    if ($requested_format[0] != APP_MEDIA_TYPE_JSON) {
+        $response_data = json_encode(getErrorUnsupportedFormat());
+        $response_code = HTTP_UNSUPPORTED_MEDIA_TYPE;
+    }
+
+    $response->getBody()->write($response_data);
+    return $response->withStatus($response_code);
+}
+
+function handleUpdateClinics(Request $request,Response $response, array $args){
+    
+    $clinics = array();
+    $response_data = array();
+    $response_code = HTTP_OK;
+    $clinic_model = new clinicModel();
+
+    $parse_data = $request->getParsedBody();
+
+    $requested_format = $request->getHeader('Accept');
+    
+    if($parse_data !== null){
+        
+        foreach($parse_data as $clinic){
+            if(!isset($clinic['clinic_id']) || !isset($clinic['clinic_name']) || !isset($clinic['clinic_address']) || !isset($clinic['clinic_details']) ){
+                $response_data = makeCustomJSONMessage("Error","Record(s) has not been updated. Some fields are empty.");
+                $response_code = HTTP_BAD_REQUEST;
+
+                $response->getBody()->write($response_data);
+                return $response->withStatus($response_code);
+            }
+            else{
+                $clinics = array("clinic_id" => $clinic['clinic_id'],"clinic_name" => $clinic['clinic_name'] , "clinic_address" => $clinic['clinic_address'],'clinic_details' => $clinic['clinic_details']);
+                $clinic_model->updateClinic($clinic);
+            }
+            
+        }
+
+        $response_data = makeCustomJSONMessage("Updated","Record(s) has been successfully updated.");
+    }
+    else{
+
+        $response_data = makeCustomJSONMessage("Error","Record(s) has not been updated. All fields are empty.");
+        $response_code = HTTP_BAD_REQUEST;
+
+    }
+
+    if ($requested_format[0] !== APP_MEDIA_TYPE_JSON) {
+        $response_data = json_encode(getErrorUnsupportedFormat());
+        $response_code = HTTP_UNSUPPORTED_MEDIA_TYPE;
+    }
+
     $response->getBody()->write($response_data);
     return $response->withStatus($response_code);
 }
